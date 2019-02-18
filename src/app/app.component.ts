4c392d0060391eb3fc9697071a62828a4eca68ca
DarwinGonzalez <alu0100812794@ull.edu.es>
2019-02-18 12:46:15 +0100

    Adding some pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { reject } from "q";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'pipes';
+  public name = "Darwin";
+
+  public array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  public PI = Math.PI;
+
+  public a = 0.234;
+
+  public salary = 1234.5;
+
+  public heroe = {
+    name: "Logan",
+    key: "Wolverine",
+    age: 550,
+    address: {
+      street: "Marvel Street, Swansea, Massachusetts, EE. UU.",
+      number: "23"
+    }
+  };
+
+  public promiseValue = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Data have come!"), 3500);
+  });
+
+  public date = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { LOCALE_ID, NgModule } from "@angular/core";
+import localeEs from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeEs);
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: "es" }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
